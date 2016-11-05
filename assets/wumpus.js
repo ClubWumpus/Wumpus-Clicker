885ccc39bcb581cfe71faa424a48eda8393a659d
PikaDude <skon7826@outlook.com>
2016-11-05 22:32:46 +1100

    0.2 - Added FEVER MODE!

--- assets/wumpus.js
+++ assets/wumpus.js
@@ -1,1 +1,39 @@
-function send(a) { socket.send(a) } var socket = new WebSocket("ws://128.199.142.235:9000"); socket.onopen = function (a) { socket.send("supbro") }, socket.onmessage = function (a) { var b = a.data; if ("ping" != b) { var c = JSON.parse(b); document.getElementById("clicks").textContent = "Wumpuses: " + c.clicks, document.getElementById("cps").textContent = "Wumpuses Per Second: " + c.cps, document.getElementById("users").textContent = "Users Online: " + c.users, document.getElementById("levelBar").style.width = 100 - (parseInt(c.max) - parseInt(c.exp)) / (10 * parseInt(c.level)) + "%", document.getElementById("level").textContent = "Level: " + c.level } }, window.addEventListener("keydown", function (a) { 32 === a.keyCode && send("click") }, !1);
+function send(message) {
+    socket.send(message);
+}
+var socket = new WebSocket("ws://128.199.142.235:9000");
+socket.onopen = function () {
+    socket.send("supbro")
+}, socket.onmessage = function (message) {
+    var data = message.data;
+    if ("ping" != data && "fever" != data) {
+        var json = JSON.parse(data);
+        document.getElementById("clicks").textContent = "Wumpuses: " + json.clicks;
+        document.getElementById("cps").textContent = "Wumpuses Per Second: " + json.cps;
+        document.getElementById("users").textContent = "SoftSoap Enthusiasts Online: " + json.users;
+        document.getElementById("levelBar").style.width = 100 - (parseInt(json.max) - parseInt(json.exp)) / (10 * parseInt(json.level)) + "%";
+        if (json.fever == "false") document.getElementById("level").textContent = "Level: " + json.level;
+        if (json.fever == "true") document.getElementById("level").textContent = "FEVER MODE! x2 WUMPUSES PER CLICK!";
+        if (json.fever == "true" && feverr == false) fever();
+    }
+}, window.addEventListener("keydown", function (a) {
+    32 === a.keyCode && send("click")
+}, !1);
+var feverr = false;
+function fever() {
+    feverr = true;
+    var audio = new Audio('assets/wumpus.mp3');
+    audio.play();
+    window.setTimeout(function () {
+        document.getElementById('title').textContent = "TAP FAST NOW!";
+        document.getElementById('title').style.animationName = 'memes';
+        document.getElementById('levelBar').style.animationName = 'bidoof, woot';
+        window.setTimeout(function () {
+            document.getElementById('title').textContent = "Click The Wumpus!";
+            document.getElementById('title').style.animationDuration = '1s';
+            document.getElementById('title').style.animationName = 'test';
+            document.getElementById('levelBar').style.animationName = '';
+            document.getElementById('levelBar').style.width = '0%';
+    }, 27000);
+    }, 3000);
+}
